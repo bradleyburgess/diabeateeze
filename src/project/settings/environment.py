c5a47bdc11427a1fb2ce33c240048aba87231e7a
Bradley Burgess <bradleyburgess@gmail.com>
2025-12-23 13:43:12 +0200

    v. 1.0.2

--- src/project/settings/environment.py
+++ src/project/settings/environment.py
@@ -19,7 +19,7 @@ env = environ.Env(
 environ.Env.read_env(BASE_DIR / ".env")
 
 # Application version
-VERSION = "1.0.1"
+VERSION = "1.0.2"
 
 # Environment type (development, testing, production)
 ENVIRONMENT = env("DJANGO_ENVIRONMENT")
