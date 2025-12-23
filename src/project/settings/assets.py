2bda43e0b1856d687c77f0dce325d8b625f6774d
Bradley Burgess <bradleyburgess@gmail.com>
2025-12-23 16:09:49 +0200

    Add WhiteNoise for static assets

--- src/project/settings/assets.py
+++ src/project/settings/assets.py
@@ -2,13 +2,24 @@
 Static files and media configuration.
 """
 
-from .environment import BASE_DIR
+from .environment import BASE_DIR, ENVIRONMENT
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "mediafiles"
+
+# WhiteNoise settings for production
+if ENVIRONMENT == "production":
+    STORAGES = {
+        "default": {
+            "BACKEND": "django.core.files.storage.FileSystemStorage",
+        },
+        "staticfiles": {
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        },
+    }
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- src/project/settings/middleware.py
+++ src/project/settings/middleware.py
@@ -4,6 +4,7 @@ Middleware configuration.
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
